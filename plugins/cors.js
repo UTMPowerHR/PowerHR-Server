870e249deb45f08493ca2e1116b886726241c8d8
NOORFATINI FARHAH BINTI NOOR ADLY <noorfatinifarhah@graduate.utm.my>
2026-07-02 01:47:36 +0800

    Add deployed Vercel client URL to CORS allowlist

--- plugins/cors.js
+++ plugins/cors.js
@@ -3,7 +3,12 @@ import fp from 'fastify-plugin';
 
 export default fp(async (fastify) => {
     fastify.register(cors, {
-        origin: ['http://localhost:5173', 'https://power-hr.vercel.app', 'https://power-hr-development.vercel.app'],
+        origin: [
+            'http://localhost:5173',
+            'https://power-hr.vercel.app',
+            'https://power-hr-development.vercel.app',
+            'https://power-hr-client.vercel.app',
+        ],
         credentials: true,
     });
 });
